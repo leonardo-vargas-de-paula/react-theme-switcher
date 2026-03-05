66a9a9255977f8a1cda288cbacb7d4df7aa5d798
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-03-05 09:19:38 -0400

    feat: adiciona checkbox para mudar tema da pagina

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import "./App.css";
 
 function App() {
   const [theme, setTheme] = useState("light");
-  return <main className={theme}>
-      oi
-    </main>;
+  const handleThemeChange = ({ target }) => {
+    setTheme(target.checked ? "dark" : "light");
+  };
+  return (
+    <main className={theme}>
+      <p>Theme Switcher</p>
+      <label>
+        <input
+          type="checkbox"
+          checked={theme === "dark"}
+          onChange={handleThemeChange}
+        />
+        Dark Mode
+      </label>
+    </main>
+  );
 }
 
 export default App;
